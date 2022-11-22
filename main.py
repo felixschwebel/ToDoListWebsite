3ebe9946c932e521cfc8bc9f18b4171d6f5dabd7
felixschwebel <schwebel.felix@gmail.com>
2022-11-22 21:39:19 +0100

    lists overview and example list finished

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
@@ -16,8 +16,24 @@ def login():
 
 
 @app.route('/list')
-def newlist():
+def list():
     return render_template('list.html')
+
+
+@app.route('/alllists')
+def all_lists():
+    return render_template('all_lists.html')
+
+
+@app.route('/newlist')
+def new_list():
+    return render_template('new_list.html')
+
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
