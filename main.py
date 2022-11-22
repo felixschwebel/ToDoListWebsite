95221d0c298464df615c0a660a8b330ac6a4332b
felixschwebel <schwebel.felix@gmail.com>
2022-11-22 13:51:52 +0100

    pages linked

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ from flask_bootstrap import Bootstrap5
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
+
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
     return render_template('login.html')
 
+
+@app.route('/list')
+def newlist():
+    return render_template('list.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
