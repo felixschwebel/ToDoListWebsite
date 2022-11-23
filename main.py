054a06fa49c993daf37c567a997642902fb5c5bb
felixschwebel <schwebel.felix@gmail.com>
2022-11-23 09:48:47 +0100

    login fields with FlaskForm and wtforms

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+import os
+
+
+class MyForm(FlaskForm):
+    email = EmailField(label='Email', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
+app.secret_key = os.environ['SECRET_KEY']
 
 
 @app.route('/')
@@ -10,9 +22,13 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    login_form = MyForm()
+    if login_form.validate_on_submit():
+        print(login_form.email.data)
+        print(login_form.password.data)
+    return render_template('login.html', form=login_form)
 
 
 @app.route('/list')
